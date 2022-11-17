4a9213c074a3ff1d09459c176c24fd70abf50556
erkelost <1256029807@qq.com>
2022-11-17 15:46:19 +0800

    chore: update viteconfig

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue';
 import plugin from 'vite-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), plugin()],
 });
