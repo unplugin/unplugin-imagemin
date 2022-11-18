06bedc646e6f60d213d11f4eff2f05d60a5b7d97
erkelost <1256029807@qq.com>
2022-11-18 17:38:14 +0800

    chore:  test 测试层成功

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import plugin from 'vite-plugin';
+import imagemin from 'unplugin-imagemin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    plugin({
-      name: 'erkelost',
-      age: 99999999999999,
-    }),
+    imagemin(),
   ],
 });
--- src/vite.ts
+++ src/vite.ts
@@ -1,3 +1,4 @@
 import unplugin from '.'
+console.log(999);
 
 export default unplugin.vite
