8e4d764a6dbef2aa59b7de4807ac20adca210a38
ERKELSOT <1256029807@qq.com>
2022-11-22 21:28:01 +0800

    chore: Type conversion test

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,22 +1,12 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/ap十大i/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <!-- <img src="./assets/image/wallhaven-p92y93.png" alt="" /> -->
+    <img src="./assets/image/wallhaven-p92y93.png" alt="" />
     <img src="./assets/image/wallhaven-gpjm3d.png" alt="" />
     <img src="./assets/image/wallhaven-2ykjxm.png" alt="" />
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/image/wallhaven-kx9rx7.png" alt="">
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import { createUnplugin } from 'unplugin';
-import * as kolorist from 'kolorist'
+import * as kolorist from 'kolorist';
 import path from 'node:path';
-import { createFilter } from '@rollup/pluginutils'
+import { createFilter } from '@rollup/pluginutils';
 import os from 'node:os';
 import * as fs from 'node:fs';
 import { ImagePool } from '@squoosh/lib';
 import { defaultOptions } from './core/types';
-import * as color from './core/log'
-import { partial } from 'filesize'
-
+import * as color from './core/log';
+import { partial } from 'filesize';
 
 import ora from 'ora';
-const size = partial({ base: 2, standard: "jedec" });
+const size = partial({ base: 2, standard: 'jedec' });
 // const spinner = ora('Loading unicorns').start();
 
 // setTimeout(() => {
@@ -23,107 +22,108 @@ const extRE = /\.(png|jpeg|jpg|webp|wb2|avif)$/i;
 export default createUnplugin<any | undefined>((options = {}): any => {
   let outputPath: string;
   let outputDir: string;
-  let publicDir: string
+  let publicDir: string;
   let files: any = [];
   const filter = createFilter(
     options.include || [extRE],
     options.exclude || [/[\\/]node_modules[\\/]/],
-  )
+  );
   return {
     name: 'unplugin-imagemin',
     apply: 'build',
     enforce: 'post',
 
     // TODO transform 修改图片上下文 如果切换文件类型 需要修改 打包之后的 file ext
-    transformInclude(id) {
-      return filter(id)
+    // transformInclude(id) {
+    //   // return filter(id);
+    // },
+    // async transform(code, id) {
+    // },
+    configResolved(resolvedConfig) {
+      outputDir = resolvedConfig.build.outDir;
+      publicDir = resolvedConfig.publicDir;
+      outputPath = path.resolve(
+        resolvedConfig.root,
+        resolvedConfig.build.outDir,
+      );
     },
-    async transform(code, id) {
-      console.log('\n');
-
-      console.log(id, '文件id');
-      // console.log(this.getAssetFileName('wallhaven-gpjm3d.png'));
-      // console.log(this.getChunkFileName('wallhaven-gpjm3d.png'));
-      // console.log(typeof code);
-      // console.log(code);
+    async generateBundle(_, bundler) {
+      Object.keys(bundler).forEach((key) => {
+        filterFile(path.resolve(outputPath, key), extRE) && files.push(key);
+      });
+      if (!files.length) {
+        return;
+      }
     },
-    // transform(code) {
-    //   return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`);
-    // },
-    // configResolved(resolvedConfig) {
-    //   outputDir = resolvedConfig.build.outDir
-    //   publicDir = resolvedConfig.publicDir
-    //   outputPath = path.resolve(
-    //     resolvedConfig.root,
-    //     resolvedConfig.build.outDir,
-    //   );
-    // },
-    // async generateBundle(_, bundler) {
-    //   Object.keys(bundler).forEach((key) => {
-    //     filterFile(path.resolve(outputPath, key), extRE) && files.push(key);
-    //   });
-    //   if (!files.length) {
-    //     return;
-    //   }
-    // },
-    // async closeBundle() {
-    //   if (!files.length) {
-    //     return;
-    //   }
-    //   const res = kolorist.blue('📦 📦 [unplugin-imagemin]')
-    //   const info = kolorist.gray('Process start ...')
-    //   console.log(res, info);
-    //   const defaultSquooshOptions = {};
-    //   Object.keys(defaultOptions).forEach(
-    //     (key) => (defaultSquooshOptions[key] = { ...defaultOptions[key] }),
-    //   );
-    //   const imagePool = new ImagePool(os.cpus().length);
-    //   const images = files.map(async (filePath: string) => {
-    //     const fileRootPath = path.resolve(outputPath, filePath);
-    //     const start = Date.now();
-    //     const image = imagePool.ingestImage(path.resolve(outputPath, filePath));
-    //     const oldSize = fs.lstatSync(fileRootPath).size;
-    //     let newSize = oldSize;
-    //     const ext = path.extname(path.resolve(outputPath, filePath)) ?? '';
-    //     const type = getUserCompressType()
-    //     await image.encode({ [type]: defaultSquooshOptions[type] });
-    //     const encodedWith = await image.encodedWith[type]
-    //     newSize = encodedWith.size;
-    //     if (newSize < oldSize) {
-    //       fs.writeFileSync(`${fileRootPath}`, encodedWith.binary);
-    //       console.log(kolorist.blue(filePath), kolorist.green(size(newSize).toString()), kolorist.magenta(`${Date.now() - start}ms`))
-    //     }
-    //     return null;
-    //   });
-    //   // console.log(images);
-    //   await Promise.all(images)
-    //   console.log(kolorist.yellow('✨ ✨ Successfully'));
-    //   imagePool.close()
-    // },
+    async closeBundle() {
+      if (!files.length) {
+        return;
+      }
+      const res = kolorist.blue('📦 📦 [unplugin-imagemin]');
+      const info = kolorist.gray('Process start ...');
+      console.log(res, info);
+      const defaultSquooshOptions = {};
+      Object.keys(defaultOptions).forEach(
+        (key) => (defaultSquooshOptions[key] = { ...defaultOptions[key] }),
+      );
+      const imagePool = new ImagePool(os.cpus().length);
+      const images = files.map(async (filePath: string) => {
+        const fileRootPath = path.resolve(outputPath, filePath);
+        const start = Date.now();
+        const image = imagePool.ingestImage(path.resolve(outputPath, filePath));
+        const oldSize = fs.lstatSync(fileRootPath).size;
+        let newSize = oldSize;
+        const ext = path.extname(path.resolve(outputPath, filePath)) ?? '';
+        const type = getUserCompressType();
+        await image.encode({ [type]: defaultSquooshOptions[type] });
+        const encodedWith = await image.encodedWith[type];
+        newSize = encodedWith.size;
+        if (newSize < oldSize) {
+          await fs.writeFileSync(`${fileRootPath.replace('png', 'webp')}`, encodedWith.binary);
+          await fs.unlinkSync(fileRootPath)
+          console.log(
+            kolorist.blue(filePath),
+            kolorist.green(size(newSize).toString()),
+            kolorist.magenta(`${Date.now() - start}ms`),
+          );
+        }
+        return null;
+      });
+      await Promise.all(images);
+      console.log(kolorist.yellow('✨ ✨ Successfully'));
+      const a = await fs.readdirSync(`${outputDir}/assets`);
+      const b = a.find((item) => {
+        return item.endsWith('.js');
+      });
+      const c = await fs.readFileSync(`${outputDir}/assets/${b}`);
+      const r = c.toString().replace(/png/g, 'webp');
+      await fs.writeFileSync(`${outputDir}/assets/${b}`, r)
+      imagePool.close();
+    },
   };
 });
-// function filterFile(
-//   file: string,
-//   filter: RegExp | ((file: string) => boolean),
-// ) {
-//   if (filter) {
-//     const isRe = isRegExp(filter);
-//     const isFn = isFunction(filter);
-//     if (isRe) {
-//       return (filter as RegExp).test(file);
-//     }
-//     if (isFn) {
-//       return (filter as (file: any) => any)(file);
-//     }
-//   }
-//   return false;
-// }
-// export const isFunction = (arg: unknown): arg is (...args: any[]) => any =>
-//   typeof arg === 'function';
+function filterFile(
+  file: string,
+  filter: RegExp | ((file: string) => boolean),
+) {
+  if (filter) {
+    const isRe = isRegExp(filter);
+    const isFn = isFunction(filter);
+    if (isRe) {
+      return (filter as RegExp).test(file);
+    }
+    if (isFn) {
+      return (filter as (file: any) => any)(file);
+    }
+  }
+  return false;
+}
+export const isFunction = (arg: unknown): arg is (...args: any[]) => any =>
+  typeof arg === 'function';
 
-// export const isRegExp = (arg: unknown): arg is RegExp =>
-//   Object.prototype.toString.call(arg) === '[object RegExp]';
+export const isRegExp = (arg: unknown): arg is RegExp =>
+  Object.prototype.toString.call(arg) === '[object RegExp]';
 
-// function getUserCompressType(type: string = 'webp') {
-//   return type
-// }
+function getUserCompressType(type: string = 'webp') {
+  return type;
+}
