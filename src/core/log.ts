14e5242fc25b3c91d8443f868dd5ec4342363963
erkelost <1256029807@qq.com>
2022-11-22 12:56:36 +0800

    chore: add log utils

---
+++ src/core/log.ts
@@ -0,0 +1,26 @@
+import * as color from 'kolorist'
+
+type colorTuple = string | number
+function colorResult<colorTuple>(args, color): void {
+  console.log(color[color](args))
+}
+export const red = (args: colorTuple) => colorResult(args, 'red')
+export const green = (res: colorTuple) => colorResult(res, 'green')
+export const gray = (res: colorTuple) => colorResult(res, 'gray')
+export const yellow = (res: colorTuple) => colorResult(res, 'yellow')
+export const blue = (res: colorTuple) => colorResult(res, 'blue')
+export const magenta = (res: colorTuple) => colorResult(res, 'blue')
+export const black = (res: string | number) => colorResult(res, 'black')
+export const cyan = (res: string | number) => colorResult(res, 'cyan')
+export const bgGreen = (res: string | number) => colorResult(res, 'bgGreen')
+export const bgRed = (res: string | number) => colorResult(res, 'bgRed')
+export const bgYellow = (res: string | number) => colorResult(res, 'bgYellow')
+export const bgGray = (res: string | number) => colorResult(res, 'bgGray')
+export const bgBlue = (res: string | number) => colorResult(res, 'bgBlue')
+export const bgMagenta = (res: string | number) => colorResult(res, 'bgMagenta')
+export const bgCyan = (res: string | number) => colorResult(res, 'bgCyan')
+
+export const complete = (name, dest, type, description) => {
+  green(`\n ${description}`)
+  cyan(`\n To get started with into "${name}.${type}"`)
+}
