60fe2a67f12bc3540a98ead863da38fb58fb8162
erkelost <1256029807@qq.com>
2022-11-24 17:38:59 +0800

    update: tsconfig line

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   entry: ['./src/*.ts'],
   format: ['esm', 'cjs'],
   target: 'node14',
-  // clean: true,
+  clean: true,
   // dts: true,
-  // splitting: true,
-  // shims: true,
+  splitting: true,
+  shims: true,
 });
