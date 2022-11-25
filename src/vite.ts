c42c381ac3909af2ecdd874bb07423cf6a363602
erkelost <1256029807@qq.com>
2022-11-25 14:49:02 +0800

    chore: resolve mjs build bundle

--- src/vite.ts
+++ src/vite.ts
@@ -1,3 +1,4 @@
 import unplugin from '.';
-
-export default unplugin.vite;
+import type { Plugin } from 'vite';
+import type { Options } from './types';
+export default unplugin.vite as (options?: Options | undefined) => Plugin;
