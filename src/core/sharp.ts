ad8a7a88ba0c1d048a73878b120623b3fb034dcf
ERKELSOT <1256029807@qq.com>
2022-11-27 17:29:23 +0800

    chore: test sharp

--- src/core/sharp.ts
+++ src/core/sharp.ts
@@ -1,7 +1,7 @@
 import sharp from 'sharp';
 
 async function initSharp(input, output) {
-  sharp(input).webp({quality: 100}).toFile(output);
+  sharp(input).webp({ quality: 100 }).toFile(output);
 }
 export function loadImage(url: string) {
   return sharp(decodeURIComponent(parseURL(url).pathname));
