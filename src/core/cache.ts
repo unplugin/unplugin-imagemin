de73e97436c53fcf597dd2bd3092a751089e134f
sdhushu <1426732052@qq.com>
2022-11-27 10:57:05 +0800

    fix: core/cache eslint error

--- src/core/cache.ts
+++ src/core/cache.ts
@@ -1,79 +1,90 @@
-import fs from "fs";
-import path from "path";
-import crypto from 'crypto'
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
 
-import pkg from '../../package.json'
+import pkg from '../../package.json';
+
 const env = process.env.NODE_ENV || 'development';
 
-const root = process.cwd()
-const cacheDirectory = path.join(root, 'node_modules', '.cache', 'unplugin-imagemin');
+const root = process.cwd();
+const cacheDirectory = path.join(
+  root,
+  'node_modules',
+  '.cache',
+  'unplugin-imagemin',
+);
 const cacheIdentifier = `unplugin-imagemin:${pkg.version} ${env}`;
-const manifestKey = path.join(cacheDirectory, 'manifest.json')
+const manifestKey = path.join(cacheDirectory, 'manifest.json');
 
 export default class Cache {
-	mainfest: any;
-	outputPath: string;
+  mainfest: any;
 
-	constructor ({ outputPath }) {
-		this.outputPath = outputPath
-		this.mainfest = getCacheManifest()
-	}
-	get (chunk) {
-		const cacheKey = getCacheKey(chunk)
-		if (!this.hasManifest(cacheKey)) {
-			return null
-		}
-		const originStats = fs.statSync(path.join(root, chunk.name));
-		const cacheStats = this.getManifest(cacheKey);
-		if (originStats.ctimeMs === cacheStats.ctimeMs) {
-			return fs.readFileSync(cacheKey)
-		}
-		return null
-	}
-	set (chunk, data) {
-		const cacheKey = getCacheKey(chunk)
-		if (!existsSync(cacheDirectory)) {
-			mkdirSync(cacheDirectory);
-		}
-		fs.writeFileSync(cacheKey, data || fs.readFileSync(path.join(this.outputPath, chunk.fileName)));
-		this.setManifest(cacheKey, fs.statSync(path.join(root, chunk.name)));
-	}
-	getManifest (key: string) {
-		return this.mainfest[key]
-	}
-	setManifest (key: string, value: object) {
-		this.mainfest[key] = value;
-		fs.writeFileSync(manifestKey, JSON.stringify(this.mainfest), 'utf-8');
-	}
-	hasManifest (key: string) {
-		return !!this.mainfest[key]
-	}
+  outputPath: string;
+
+  constructor({ outputPath }) {
+    this.outputPath = outputPath;
+    this.mainfest = getCacheManifest();
+  }
+
+  get(chunk) {
+    const cacheKey = getCacheKey(chunk);
+    if (!this.hasManifest(cacheKey)) {
+      return null;
+    }
+    const originStats = fs.statSync(path.join(root, chunk.name));
+    const cacheStats = this.getManifest(cacheKey);
+    if (originStats.ctimeMs === cacheStats.ctimeMs) {
+      return fs.readFileSync(cacheKey);
+    }
+    return null;
+  }
+
+  set(chunk, data) {
+    const cacheKey = getCacheKey(chunk);
+    if (!existsSync(cacheDirectory)) {
+      mkdirSync(cacheDirectory);
+    }
+    fs.writeFileSync(
+      cacheKey,
+      data || fs.readFileSync(path.join(this.outputPath, chunk.fileName)),
+    );
+    this.setManifest(cacheKey, fs.statSync(path.join(root, chunk.name)));
+  }
+
+  getManifest(key: string) {
+    return this.mainfest[key];
+  }
+
+  setManifest(key: string, value: object) {
+    this.mainfest[key] = value;
+    fs.writeFileSync(manifestKey, JSON.stringify(this.mainfest), 'utf-8');
+  }
+
+  hasManifest(key: string) {
+    return Boolean(this.mainfest[key]);
+  }
 }
 
 function getCacheManifest(): object {
-	if (!existsSync(manifestKey)) {
-		return {}
-	}
-	const content = JSON.parse(fs.readFileSync(manifestKey, 'utf-8')) || {}
-	return content
+  if (!existsSync(manifestKey)) {
+    return {};
+  }
+  return JSON.parse(fs.readFileSync(manifestKey, 'utf-8')) || {};
 }
 
 function getCacheKey(chunk): string {
-	const hash = digest(`${cacheIdentifier}\n${chunk.name}`);
-	return path.join(cacheDirectory, `${hash}`);
+  const hash = digest(`${cacheIdentifier}\n${chunk.name}`);
+  return path.join(cacheDirectory, `${hash}`);
 }
 
-function existsSync(path: string): boolean {
-	return fs.existsSync(path)
+function existsSync(existsPath: string): boolean {
+  return fs.existsSync(existsPath);
 }
 
-function mkdirSync(path: string): void {
-	fs.mkdirSync(path, { recursive: true })
+function mkdirSync(mkdirPath: string): void {
+  fs.mkdirSync(mkdirPath, { recursive: true });
 }
 
 function digest(str: string): string {
-  return crypto
-    .createHash('md5')
-    .update(str)
-    .digest('hex');
+  return crypto.createHash('md5').update(str).digest('hex');
 }
