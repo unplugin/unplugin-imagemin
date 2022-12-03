ab36ad7452b85bb61b9aa03addc50145a338dd75
ERKELSOT <1256029807@qq.com>
2022-12-03 17:42:37 +0800

    chore: Compatible webpack default

---
+++ src/core/unplugin.ts
@@ -0,0 +1,40 @@
+import { resolveDefaultOptions } from './compressOptions';
+import type { PluginOptions } from './types';
+import { createUnplugin } from 'unplugin';
+import Context from './context';
+
+// TODO conversion 一套接口问题
+// TODO 提取出结构赋值
+export default createUnplugin<PluginOptions | undefined>(
+  (options = {}): any => {
+    const ctx = new Context();
+    // eslint-disable-next-line prefer-object-spread
+    const assignOptions = Object.assign({}, resolveDefaultOptions, options);
+    return {
+      name: 'unplugin-imagemin',
+      apply: 'build',
+      enforce: 'pre',
+      async configResolved(config) {
+        ctx.handleMergeOptionHook({ ...config, options: assignOptions });
+      },
+      async load(id) {
+        if (options.beforeBundle) {
+          const imageModule = ctx.loadBundleHook(id);
+          if (imageModule) {
+            return imageModule;
+          }
+        }
+      },
+      async generateBundle(_, bundler) {
+        if (options.beforeBundle) {
+          await ctx.generateBundleHook(bundler);
+        } else {
+          ctx.TransformChunksHook(bundler);
+        }
+      },
+      async closeBundle() {
+        ctx.closeBundleHook();
+      },
+    };
+  },
+);
--- src/vite.ts
+++ src/vite.ts
@@ -1,4 +1,4 @@
-import unplugin from './index';
+import unplugin from '.';
 import type { Plugin } from 'vite';
 import type { Options } from './types';
 export default unplugin.vite as (options?: Options | undefined) => Plugin;
