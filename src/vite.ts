b523b1b0c2a109498de28f3ef8bfc62339376593
ERKELSOT <1256029807@qq.com>
2022-12-04 13:59:15 +0800

    chore: type

--- src/vite.ts
+++ src/vite.ts
@@ -1,4 +1,4 @@
 import unplugin from '.';
 import type { Plugin } from 'vite';
-import type { Options } from './core/types';
-export default unplugin.vite as (options?: Options | undefined) => Plugin;
+// import type { Options } from './core/types';
+export default unplugin.vite as (options?: any | undefined) => Plugin;
